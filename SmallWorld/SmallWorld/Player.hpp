f8cdc9a58a0d79b2557dc7c3799749df4ecddbbe
Charles <chanc09@gmail.com>
2018-02-07 22:27:49 -0500

    first cpp file and to do list

---
+++ SmallWorld/SmallWorld/Player.hpp
@@ -0,0 +1,14 @@
+//
+//  Player.hpp
+//  SmallWorld
+//
+//  Created by Charles  Chan on 2018-02-07.
+//  Copyright © 2018 Charles Chan. All rights reserved.
+//
+
+#ifndef Player_hpp
+#define Player_hpp
+
+#include <stdio.h>
+
+#endif /* Player_hpp */
