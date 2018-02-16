66ed5bbc60a751ebc28f78bd1b4a2b311b117b2a
Charles <chanc09@gmail.com>
2018-02-15 23:50:22 -0500

    #4 done, just need to merge

---
+++ SmallWorld/SmallWorld/Dice.cpp
@@ -0,0 +1,90 @@
+//
+//  Dice.cpp
+//  SmallWorld
+//
+//  Created by Charles  Chan on 2018-02-15.
+//  Copyright © 2018 Charles Chan. All rights reserved.
+//
+
+#include <iostream>
+
+#include "Dice.hpp"
+
+Dice::Dice(){
+    //cout<<"DICE WAS JUST CREATED"<<endl;
+}
+
+Dice::~Dice(){
+    //container = nullptr;
+    
+}
+void Dice::percentage(){ //returns the percentage of each numbers rolled
+    cout<< "\nYou have rolled 1:  " << container[1] << " times --> "<<(container[1]/container[0])*100 <<"%\n";
+    cout<< "You have rolled 2:  " << container[2] << " times --> "<<(container[2]/container[0])*100 <<"%\n";
+    cout<< "You have rolled 3:  " << container[3] << " times --> "<<(container[3]/container[0])*100 <<"%\n";
+    cout<< "You have rolled a total of:  " << container[0] << endl;
+}
+
+void Dice::storeResults(int i){
+    if (i==1){
+        container[1]=container[1]+1;
+        
+    }
+    else if (i==2){
+        container[2]=container[2]+1;
+        
+    }
+    else if (i==3){
+        container[3]=container[3]+1;
+    }
+    container[0] = container[0] + 1;
+}
+
+void Dice::rollingDice(int tokens, int regionTokens){ //rolling the dice method
+    int numConquer;
+    int showResults;
+    int x = 0;
+    //int diceResult;
+    cout << "=================="<<endl;
+    cout << "Final conquest" << endl;
+    cout << "=================="<<endl;
+    cout << "Rolling dice"<<endl;
+    //for the purposes of the demo I created a while loop to continue rolling
+    while(x==0){
+        diceResult=random();
+        storeResults(diceResult);
+        cout << "The dice you rolled was ";
+        cout << diceResult << endl;
+        cout << "In your hand, you have ";
+        cout << tokens;
+        cout << " tokens remaining." <<endl;
+        cout << "Would you like to conquer the territory? 0 for yes" <<endl;
+        cin >> numConquer;
+        if(numConquer == 0){
+            tokens = tokens - regionTokens;
+        }
+        cin.clear();
+        cin.ignore();
+        
+        cout << "In your hand, you have ";
+        cout << tokens;
+        cout << " tokens remaining." <<endl;
+        
+        cout <<"Press 1 to see how many dices of each you have rolled so far. Press 0 to continue\n";
+        cin >> showResults;
+        if(showResults==1){
+            percentage();
+        }
+        cin.clear();
+        cin.ignore();
+        
+        cout << "Stop rolling press 1, continue press 0" << endl;
+        cin >> x;
+        cin.clear();
+        cin.ignore();
+    }
+}
+
+int Dice::random(){  //random method to generate a number between 1-3 for the dice.
+    return (rand()%3 +1);
+}
---
+++ SmallWorld/SmallWorld/DiceDriver.cpp
@@ -0,0 +1,28 @@
+//
+//  DiceDriver.cpp
+//  SmallWorld
+//
+//  Created by Charles  Chan on 2018-02-15.
+//  Copyright © 2018 Charles Chan. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+#include "Dice.hpp"
+using namespace std;
+
+//DRIVER for dice
+int main(int argc, const char * argv[]) {
+    Dice* d1 = new Dice();
+    Dice* d2 = new Dice();
+    int tokens1 = 50;
+    int tokens2 = 50;
+    int regionTokens[] = {1,2,3,4,5,6,7};
+    
+    d1->rollingDice(tokens1, regionTokens[1]);
+    d2->rollingDice(tokens2, regionTokens[6]);
+    
+    
+    
+    return 0;
+}
